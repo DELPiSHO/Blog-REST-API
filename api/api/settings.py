15356d9cfbba6b7df593feef7012d3daeff9bdc6
Eugene Denisov <zhekadeni98@mail.ru>
2021-07-04 17:33:39 +0200

    Update settings.py

--- api/api/settings.py
+++ api/api/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-y_0h#k%$by+6nrvofd$ok_)qpvo3vq5=ipn_$15g7si70xk3-r'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -164,8 +164,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'fanatik1798@gmail.com'
-EMAIL_HOST_PASSWORD = 'onswakvlopquwewy'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 
 
 ####################################
